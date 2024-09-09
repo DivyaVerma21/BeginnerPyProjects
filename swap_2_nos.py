851eb3261406d37a19652db4472a0c747414a338
DivyaVerma21 <divya.hiof@gmail.com>
2024-09-09 23:34:37 +0200

    Python program to ask user to enter two numbers and swap them without using a temporary variable.

---
+++ swap_2_nos.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+print("before swap value of A :", a)
+print("before swap value of B :", b)
+a = a+b
+b = a-b
+a = a-b
+print("after swap value of A :", a)
+print("after swap value of B :", b)
