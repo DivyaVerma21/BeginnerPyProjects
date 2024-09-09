d0c78b2eef5cb343c106b3cf5afaca32ce72fc98
DivyaVerma21 <divya.hiof@gmail.com>
2024-09-09 22:51:40 +0200

    1st python program

---
+++ main.py
@@ -0,0 +1,10 @@
+a=input("Enterthe first number")
+b=input("Enterthe second number")
+a1=int(a)
+a2=int(b)
+c=a1+a2
+print(c)
+a=int(input("Enterthe first number"))
+b=int(input("Enterthe second number"))
+c=a+b
+print(c)
