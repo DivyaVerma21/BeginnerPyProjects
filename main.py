4b3ef2d7eee742aa7fe91c880ee474787d7ddcb4
DivyaVerma21 <divya.hiof@gmail.com>
2024-09-09 23:26:19 +0200

    Python program to ask user to enter two numbers and add them

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-a=input("Enterthe first number")
-b=input("Enterthe second number")
-a1=int(a)
-a2=int(b)
-c=a1+a2
+
+a = input("Enter the first number")
+b = input("Enter the second number")
+a1 = int(a)
+a2 = int(b)
+c = a1+a2
 print(c)
-a=int(input("Enterthe first number"))
-b=int(input("Enterthe second number"))
-c=a+b
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+c = a+b
 print(c)
